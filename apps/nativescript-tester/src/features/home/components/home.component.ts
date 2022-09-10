9aa190c0cdbd8afc6d2b203721628611b037cd88
Smaïl Hammour <smail.hammour@pegus.digital>
2022-09-10 08:20:29 +0200

    Revert changes to animation

--- apps/nativescript-tester/src/features/home/components/home.component.ts
+++ apps/nativescript-tester/src/features/home/components/home.component.ts
@@ -1,37 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { fadeAnimation } from '../../../fade.animation';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import {Component, OnInit} from '@angular/core';
+import {fadeAnimation} from '../../../fade.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  animations: [
-    trigger('fade', [
-      // ...
-      state(
-        'void => *',
-        style({
-          opacity: 1,
-          backgroundColor: 'yellow',
-        })
-      ),
-      state(
-        '* => void',
-        style({
-          opacity: 0.8,
-          backgroundColor: 'blue',
-        })
-      ),
-      transition('* => void', [animate('1s')]),
-      transition('void => *', [animate('0.5s')]),
-    ]),
-  ],
+  animations: [fadeAnimation('20000ms')],
 })
 export class HomeComponent implements OnInit {
   canShowContent = false;
